e9b0ab5e8494b92f68552071b158f316f602f230
Yakomori <samuel.henault@gmail.com>
2021-11-11 10:23:16 -0500

    Update DefaultLayout.js

--- src/components/DefaultLayout.js
+++ src/components/DefaultLayout.js
@@ -9,16 +9,24 @@ const DefaultLayout = ({ title, backgroundImageUrl, children, handleChange }) =>
   return (
     <>
       <Helmet>
+        {/*<!-- Primary Meta Tags -->*/}
         <title>Devotech PC</title>
+        <meta name="title" content="Devotech PC" />
+        <meta name="description" content="Devotech PC is the perfect place for your computer needs." />
+
+        {/*<!-- Open Graph / Facebook -->*/}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.devotechpc.com/" />
         <meta property="og:title" content="Devotech PC" />
-        <meta property="og:image" itemProp="image" content={HeaderBackgroundDesktop} />
-        <meta property="og:description" content="Devotech PC is the perfect place for your computer needs."/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://www.devotechpc.com/"/>
-        <meta property="twitter:title" content="Devotech PC"/>
-        <meta property="twitter:description" content="Devotech PC is the perfect place for your computer needs."/>
-        <meta property="twitter:image" itemProp="image"  content={HeaderBackgroundDesktop}/>
-        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="og:description" content="Devotech PC is the perfect place for your computer needs." />
+        <meta property="og:image" content={HeaderBackgroundDesktop} />
+
+        {/*<!-- Twitter -->*/}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.devotechpc.com/" />
+        <meta property="twitter:title" content="Devotech PC" />
+        <meta property="twitter:description" content="Devotech PC is the perfect place for your computer needs." />
+        <meta property="twitter:image" content={HeaderBackgroundDesktop} />
       </Helmet>
       <Header title={title} backgroundImageUrl={backgroundImageUrl} handleChange={handleChange} />
       {children}
